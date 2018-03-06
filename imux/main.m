7d1da386fd861faae393f91ea7e01e21d1b53752
Alexander Smirnov <alexandr.smirnov@corp.mail.ru>
2018-03-06 17:32:33 +0300

    support for multiple devices

--- imux/main.m
+++ imux/main.m
@@ -13,6 +13,8 @@
 
 volatile am_device_t target_device = NULL;
 
+NSString *target_device_id = nil;
+
 NSString* am_deviceIdentifier(am_device_t device) {
     CFStringRef deviceIdentifier = AMDeviceCopyDeviceIdentifier(device);
     return [((NSString*)deviceIdentifier) autorelease];
@@ -35,7 +37,16 @@ void am_notification_handler(struct am_device_notification_callback_info * notif
                 return;
             }
             
-            target_device = notificationInfo->dev;
+            if(target_device_id == nil) {
+                printf("stick with first connected device [%s]\n", [deviceIdentifier cStringUsingEncoding: NSASCIIStringEncoding]);
+                target_device_id = deviceIdentifier;
+            }
+            
+            if([target_device_id isEqualToString: deviceIdentifier]) {
+                
+                
+                target_device = notificationInfo->dev;
+            }
         }
         if(notificationInfo->msg == ADNCI_MSG_DISCONNECTED) {
             printf("[%s] disconnected\n", deviceIdentifier.UTF8String);
@@ -51,6 +62,23 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         NSRunLoop* runLoop = [NSRunLoop currentRunLoop];
+
+        if(argc < 3) {
+            fprintf(stderr, "usage: imux <localport> <remote port> [<device id>]\n");
+            exit(1);
+        }
+        
+        uint32_t localport = atoi(argv[1]);
+        uint32_t remoteport = atoi(argv[2]);
+        
+        printf("local port    : %u\n",localport);
+        printf("remote port   : %u\n",remoteport);
+        
+        if(argc == 4) {
+            target_device_id = [NSString stringWithCString: argv[3] encoding: NSASCIIStringEncoding];
+            
+            printf("target device : %s\n", argv[3]);
+        }
         
         am_device_callbacks_t callbacks;
         int result = AMDeviceNotificationSubscribe(am_notification_handler, 0, 0, 0, &callbacks);
@@ -60,10 +88,10 @@ int main(int argc, const char * argv[])
             exit(-1);
         }
         
-        int server_socket = util_start_tcp_server(31337);
+        int server_socket = util_start_tcp_server(localport);
         
         if(server_socket <= 0) {
-            fprintf(stderr,"[-] failed to start local tcp server at port '%d'", 31337);
+            fprintf(stderr,"[-] failed to start local tcp server at port '%d'", localport);
             exit(-1);
         }
         
@@ -76,13 +104,13 @@ int main(int argc, const char * argv[])
                         return;
                     printf("[+] Got a local connection\n");
                     muxconn_t connection_id = AMDeviceGetConnectionID(device);
-                    int ret = USBMuxConnectByPort(connection_id, htons(22), &iphone_socket);
+                    int ret = USBMuxConnectByPort(connection_id, htons(remoteport), &iphone_socket);
                     
                     if(ret!=0) {
                         fprintf(stderr,"[-] failed to perform mux connect on device");
                         return;
                     }
-                    printf("[+] Tying local connection with iDevice\n");
+                    printf("[+] Tying local connection with iDevice[%s]\n",[target_device_id cStringUsingEncoding: NSASCIIStringEncoding]);
                     util_tie_in_sockets(client_socket, iphone_socket);
                 });
             }
